c3c90299e178cd8554504b55799886eca4a69f1c
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2021-09-28 11:37:06 +0700

    Add files via upload

---
+++ b3s vd2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+	 int a,b,c sum;
+	 peintf("\nEnter any three number: ");
+	 scanf("%d %d %d", &a,%b,%c);
+	 
+	 sum = a + b + c;
+	 
+	 printf("\n sum=%d", sum)
+}
